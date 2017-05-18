266b1add571ef7a05e3c78830fbfbeced5e394df
EklavyaM <eklavyam96@gmail.com>
2017-05-18 13:43:23 +0530

    Add files via upload

---
+++ Braid-like Game Dev Beginner Project/main.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "Game.h"
+
+int main() {
+
+	Game game;
+	game.run();
+
+	return 0;
+}
+
---
+++ Braid-like Game Dev Beginner Project/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+#include <map>
+#include <iostream>
+#include <cassert>
+#include <bitset>
+#include <vector>
